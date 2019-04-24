10d0cdc03e32060559b45152ae0764ab92bd8b3a
IplayLF2 <914637118@qq.com>
2019-04-24 23:49:34 +0800

    格式化文档

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,10 @@
-﻿const path = require('path');
-const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
+﻿const path = require("path");
+const merge = require("webpack-merge");
+const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-    mode: 'production',
-    output:{
-        path: path.resolve(__dirname, 'dist')
-    }
+  mode: "production",
+  output: {
+    path: path.resolve(__dirname, "dist")
+  }
 });
